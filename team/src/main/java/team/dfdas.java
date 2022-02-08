23ddb040fba88cb791af51de09079e2d44171643
DESKTOP-R8KS7H9\kjca28 <kimtea2004@hanmail.net>
2022-02-08 18:14:38 +0900

    asd

---
+++ team/src/main/java/team/dfdas.java
@@ -0,0 +1,12 @@
+package team;
+
+public class dfdas {
+		public static void main(String[] args) {
+			
+			
+			int a = 0;
+			int b= 1 ;
+			int[] ar = {a,b};
+			System.out.println(ar[0]);
+		}
+}
