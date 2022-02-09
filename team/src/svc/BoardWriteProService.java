ff08508d8427b690003ee0e5aa762da0ac743f6f
TaeWan <TaeWan@DESKTOP-PU57EAB>
2022-02-09 21:10:13 +0900

    2/9 집에서 한 것

--- team/src/svc/BoardWriteProService.java
+++ team/src/svc/BoardWriteProService.java
@@ -1,11 +1,16 @@
 package svc;
 
+import java.sql.Connection;
+
 import vo.BoardBean;
+import static db.JdbcUtil.*;
 
 public class BoardWriteProService {
 
-	public boolean registArticle(BoardBean boardBean) {
-		// TODO Auto-generated method stub
+	public boolean registArticle(BoardBean boardBean) throws Exception {
+		
+		boolean isWriteSuccess=false;
+		Connection con=getConnection();
 		return false;
 	}
 
